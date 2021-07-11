c6be1376f56ca30fe7d3074588765e835e4b4526
Jiang5ai <1399377871@qq.com>
2021-07-11 20:50:41 +0800

    Model增加verbose_name

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,13 @@
 import datetime
 
-
 from django.db import models
 from django.utils import timezone
 
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, verbose_name='问题描述')
+    pub_date = models.DateTimeField(verbose_name='发布日期')
 
     def __str__(self):
         return self.question_text
@@ -19,8 +18,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200, verbose_name='选项')
+    votes = models.IntegerField(default=0, verbose_name='得票数')
 
     def __str__(self):
         return self.choice_text
